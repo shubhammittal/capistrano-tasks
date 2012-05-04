c6e74d735863bb1195f89036a124ae56623bbe0b
Arnold Noronha <arnstein87@gmail.com>
2012-05-04 16:56:27 -0400

    just ensuring that _cset is the same as set.

--- spec/unicorn_spec.rb
+++ spec/unicorn_spec.rb
@@ -2,9 +2,13 @@ require "test_helper"
 
 describe CapistranoTasks::Unicorn do
   before do
+    setup_configuration
+  end
+
+  def setup_configuration(params = {})
     @configuration = Capistrano::Configuration.new
     @configuration.set :current_path, "/home/arnold/builds/capistrano-tasks/tmp"
-    CapistranoTasks::Unicorn.load_into(@configuration, "test_c")
+    CapistranoTasks::Unicorn.load_into(@configuration, "test_c", params)
   end
 
   # random test for the sake of seeing how tests work
@@ -15,5 +19,10 @@ describe CapistranoTasks::Unicorn do
   it "should define a few tasks" do
     @configuration.find_task("test_c:start").wont_be_nil
     @configuration.find_task("test_c:dfdfdfd").must_be_nil
+  end
+
+  it "should be able to read param variables" do
+    setup_configuration(:bootup_timeout => 60)
+    @configuration.fetch(:bootup_timeout).must_equal 60
   end
 end
